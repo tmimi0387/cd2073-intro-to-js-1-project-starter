f654a644fb6e0212f3eb2866ca64221243100d54
tmimi0387 <66104801+tmimi0387@users.noreply.github.com>
2023-06-06 22:53:50 -0400

    Update script.js
    docs
    
    declared all the necessary functions and finished the algorithms for most of them

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -9,17 +9,35 @@
   - image: picture of product (url string)
 */
 
-const products = [
-  {name: "Cherry", price: 5, quantity: 0, productId: 1, image: "../images/cherry.jpg"},
-  {name: "Orange", price: 10, quantity: 0, productId: 2, image: "../images/orange.jpg"},
-  {name: "Strawberry", price: 15, quantity: 0, productId: 3, image: "../images/strawberry.jpg"}
-];
-
 /* Images provided in /images folder. All images from Unsplash.com
   - cherry.jpg by Mae Mu
   - orange.jpg by Mae Mu
   - strawberry.jpg by Allec Gomes
 */
+
+const products = [
+  {
+    name: "Cherry",
+    price: 5,
+    quantity: 0,
+    productId: 1,
+    image: "../images/cherry.jpg"
+  },
+  {
+    name: "Orange",
+    price: 10,
+    quantity: 0,
+    productId: 2,
+    image: "../images/orange.jpg"
+  },
+  {
+    name: "Strawberry",
+    price: 15,
+    quantity: 0,
+    productId: 3,
+    image: "../images/strawberry.jpg"
+  }
+];
 
 /* Declare an empty array named cart to hold the items in the cart */
 
@@ -34,20 +52,26 @@ const cart = [];
 function addProductToCart(productId) {
   products.forEach(product => {
     if (productId === product.productId) {
-      if (!cart.includes('productId')) {
+      product.quantity += 1;
+      if (!cart.includes(product)) {
         cart.push(product);
-      };
-      product.quantity = product.quantity + 1;
-    };
+      }
+    }
   });
 };
-
-addProductToCart(2);
 
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
+
+function increaseQuantity(productId) {
+  products.forEach(product => {
+    if (productId === product.productId) {
+      product.quantity +=1;
+    }
+  })
+};
 
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
@@ -55,23 +79,62 @@ addProductToCart(2);
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
 
+function decreaseQuantity(productId) {
+  products.forEach(product => {
+    if (productId === product.productId) {
+      product.quantity -= 1;
+      if (product.quantity === 0) {
+        let index = cart.indexOf(product);
+        cart.splice(index, 1);
+      }
+    };
+  });
+};
+
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
 
+function removeProductFromCart(productId) {
+  products.forEach(product => {
+    if (productId === product.productId) {
+      product.quantity = 0;
+      let index = cart.indexOf(product);
+      cart.splice(index, 1);
+    }
+  });
+};
+
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
 
+function cartTotal() {
+  let total = 0;
+  cart.forEach(product => {
+    let itemTotal = product.price * product.quantity;
+    total += itemTotal;
+  });
+  return total;
+};
+
 /* Create a function called emptyCart that empties the products from the cart */
+
+function emptyCart() {
+  cart = [];
+};
 
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
+
+function pay(amount) {
+
+};
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
@@ -86,12 +149,12 @@ module.exports = {
   products,
   cart,
   addProductToCart,
-  /* increaseQuantity,
+  increaseQuantity,
   decreaseQuantity,
   removeProductFromCart,
   cartTotal,
   pay, 
-  emptyCart, */
+  emptyCart,
    // Uncomment the following line if completing the currency converter bonus
    // currency
 }
